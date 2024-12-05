53b29e431dcaaa4ce48ece0f2c12c4153532b04f
Shihab Mahamud <shihab4t@gmail.com>
2024-12-06 00:32:22 +0600

    remove handle replacement input file

--- lua/sr/search/search_and_replace.lua
+++ lua/sr/search/search_and_replace.lua
@@ -1,9 +1,17 @@
--- Main entry point
+local function handle_replacement_input(search_pattern)
+    vim.ui.input({ prompt = "Replace with: " }, function(replacement)
+        if not replacement then
+            return
+        end
+        require("sr.search.show_picker")(search_pattern, replacement)
+    end)
+end
+
 return function()
     vim.ui.input({ prompt = "Search pattern: " }, function(search_pattern)
         if not search_pattern then
             return
         end
-        require("sr.search.handle_replacement_input")(search_pattern)
+        handle_replacement_input(search_pattern)
     end)
 end
